79c91f5137fb0262bb57addd312c9df6d4bc832c
Vivek Verma <vivnps.verma@gmail.com>
2018-12-15 08:37:23 -0800

    Adding Graphs

--- mean_squared_error.py
+++ mean_squared_error.py
@@ -52,33 +52,28 @@ class GradientDescent(Scene):
 
 class AbsX(GraphScene):
     CONFIG = {
-        "x_min": -10,
-        "x_max": 10,
-        "y_min": -1.5,
-        "y_max": 1.5,
+        "x_min": -5,
+        "x_max": 5,
+        "y_min": -5,
+        "y_max": 5,
         "graph_origin": ORIGIN,
         "function_color": RED,
         "axes_color": GREEN,
-        "x_labeled_nums": range(-10, 12, 2),
+        "x_labeled_nums": range(-5, 6, 2),
 
     }
 
     def construct(self):
         self.setup_axes(animate=True)
         func_graph = self.get_graph(self.func_to_graph, self.function_color)
-        func_graph2 = self.get_graph(self.func_to_graph2)
-        vert_line = self.get_vertical_line_to_graph(TAU, func_graph, color=YELLOW)
-        graph_lab = self.get_graph_label(func_graph, label="\\cos(x)")
-        graph_lab2 = self.get_graph_label(func_graph2, label="\\sin(x)", x_val=-10, direction=UP / 2)
-        two_pi = TexMobject("x = 2 \\pi")
-        label_coord = self.input_to_graph_point(TAU, func_graph)
+        graph_lab = self.get_graph_label(func_graph, label="y = |x|")
+        two_pi = TexMobject("x = 0")
+        label_coord = self.input_to_graph_point(0, func_graph)
         two_pi.next_to(label_coord, RIGHT + UP)
 
-        self.play(ShowCreation(func_graph), ShowCreation(func_graph2))
-        self.play(ShowCreation(vert_line), ShowCreation(graph_lab), ShowCreation(graph_lab2), ShowCreation(two_pi))
+        self.play(ShowCreation(func_graph))
+        self.play(ShowCreation(graph_lab), ShowCreation(two_pi))
+        self.wait(2)
 
     def func_to_graph(self, x):
-        return np.cos(x)
-
-    def func_to_graph2(self, x):
-        return np.sin(x)
+        return np.abs(x)
