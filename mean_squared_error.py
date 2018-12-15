31d1d53b31a502535e7aa8c86ad81135844cda75
Vivek Verma <vivnps.verma@gmail.com>
2018-12-14 20:00:14 -0800

    Adding Things

--- mean_squared_error.py
+++ mean_squared_error.py
@@ -3,5 +3,21 @@ from manim import *
 
 class Intro(Scene):
     def construct(self):
-        text = TextMobject("Why do we use Mean Squared Error?", tex_to_color_map={"*": YELLOW})
+        text = TextMobject("Why do we use Mean Squared Error?", tex_to_color_map={"Mean Squared Error": YELLOW})
+        text.scale(1.5)
         self.play(Write(text))
+
+
+class CostFunction(Scene):
+    def construct(self):
+        text=TextMobject("Cost Function", tex_to_color_map={"Cost Function": YELLOW})
+        text2=TextMobject("not")
+        eq2 = TextMobject("$\\frac{1}{2m} \\sum_{i=1}^{m} (h_\\theta (x^{(i)}) - y^{(i)})^2 $")
+        eq1 = TextMobject("$\\frac{1}{2m} \\sum_{i=1}^{m} (h_\\theta (x^{(i)}) - y^{(i)})^2 $")
+        text.scale(2)
+        eq1.scale(2.5)
+        text.move_to(3*UP)
+        self.play(Write(text))
+        self.play(Transform(text,eq1))
+        self.play()
+        self.wait(2)
