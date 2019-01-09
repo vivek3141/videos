20cb196c80b705865383be546ea0d9dad93a086e
Vivek Verma <vivnps.verma@gmail.com>
2019-01-08 21:27:00 -0800

    Fixing Bugs

--- create_bash_file.py
+++ create_bash_file.py
@@ -3,8 +3,11 @@ import sys, inspect
 
 file = str(input("Enter File Name:"))
 
+with open(f"{file}.sh", "w") as f:
+    f.write("")
+
 with open(f"{file}.sh", "a+") as f:
     f.write("#!/usr/bin/env bash")
-    for name, obj in inspect.getmembers(sys.modules[__name__]):
+    for name, obj in inspect.getmembers(sys.modules[f"{file}.py"]):
         if inspect.isclass(obj):
             f.write(f"python3 extract_scene.py mean_squared_error.py {obj}")
---
+++ streamer_ai.sh
@@ -0,0 +1,1 @@
+#!/usr/bin/env bash#!/usr/bin/env bash
