e5bf149646b24c2bded84aac4bc02d0daf8837ef
Vivek Verma <vivnps.verma@gmail.com>
2019-01-30 22:36:27 -0800

    Adding Line Integral

--- line_integral.py
+++ line_integral.py
@@ -2,24 +2,9 @@ from manim import *
 
 
 class LineIntegral(ThreeDScene):
-    CONFIG = {
-        "u_min": 0,
-        "u_max": 1,
-        "v_min": 0,
-        "v_max": 1,
-        "resolution": 6,
-        "surface_piece_config": {},
-        "fill_color": BLUE_D,
-        "fill_opacity": 1.0,
-        "checkerboard_colors": [BLUE_D, BLUE_E],
-        "stroke_color": LIGHT_GREY,
-        "stroke_width": 0.5,
-        "should_make_jagged": False,
-        "pre_function_handle_to_anchor_scale_factor": 0.00001,
-    }
     def construct(self):
         self.set_camera_orientation(0.8 * np.pi / 2, -0.45 * np.pi)
-        surface = ParametricSurface(self.func)
+        surface = ThreeDVMobject
         self.play(ShowCreation(ThreeDAxes))
         self.play(ShowCreation(surface))
         self.wait()
@@ -31,5 +16,5 @@ class LineIntegral(ThreeDScene):
         return np.array([
             u,
             v,
-            u**2 + v**2
+            u ** 2 + v ** 2
         ])
