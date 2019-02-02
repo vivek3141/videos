1c788eac56b654ff717bfff76397a43fe98e3436
Vivek Verma <vivnps.verma@gmail.com>
2019-02-01 23:38:47 -0800

    Intro

--- streamer_ai.py
+++ streamer_ai.py
@@ -3,7 +3,16 @@ from manim import *
 
 class Intro(Scene):
     def construct(self):
-        pass
+        title = TextMobject("Super Mario NEAT")
+        title.scale(1.5)
+        title.to_edge(UP)
+        rect = ScreenRectangle(height=6)
+        rect.next_to(title, DOWN)
+        self.play(
+            FadeInFromDown(title),
+            ShowCreation(rect)
+        )
+        self.wait(2)
 
 
 class Mario(Scene):
