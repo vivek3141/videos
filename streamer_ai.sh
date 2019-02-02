caaea782d70582efe28011c70699fb620536588e
Vivek Verma <vivnps.verma@gmail.com>
2019-02-02 01:00:54 -0800

    Activation Func

--- streamer_ai.sh
+++ streamer_ai.sh
@@ -1,1 +1,6 @@
 #!/usr/bin/env bash
+python3 extract_scene.py streamer_ai.py Intro
+python3 extract_scene.py streamer_ai.py InputANN
+python3 extract_scene.py streamer_ai.py NeuralNetwork
+python3 extract_scene.py streamer_ai.py ActivationFunctions
+
