0217d5cfbca4455d5822e3cbb149ded098c65163
Vivek Verma <vivnps.verma@gmail.com>
2019-02-02 11:07:20 -0800

    Updating NetworkMobject

---
+++ network.py
@@ -0,0 +1,104 @@
+from manim import *
+
+
+class NeuralNetworkMobject(VGroup):
+    CONFIG = {
+        "neuron_radius": 0.15,
+        "neuron_to_neuron_buff": MED_SMALL_BUFF,
+        "layer_to_layer_buff": LARGE_BUFF,
+        "neuron_stroke_color": BLUE,
+        "neuron_stroke_width": 3,
+        "neuron_fill_color": GREEN,
+        "edge_color": LIGHT_GREY,
+        "edge_stroke_width": 2,
+        "edge_propogation_color": YELLOW,
+        "edge_propogation_time": 1,
+        "max_shown_neurons": 16,
+        "brace_for_large_layers": True,
+        "average_shown_activation_of_large_layer": True,
+        "include_output_labels": False,
+    }
+
+    def __init__(self, neural_network, **kwargs):
+        VGroup.__init__(self, **kwargs)
+        self.layer_sizes = neural_network
+        self.add_neurons()
+        self.add_edges()
+
+    def add_neurons(self):
+        layers = VGroup(*[
+            self.get_layer(size)
+            for size in self.layer_sizes
+        ])
+        layers.arrange_submobjects(RIGHT, buff=self.layer_to_layer_buff)
+        self.layers = layers
+        self.add(self.layers)
+        if self.include_output_labels:
+            self.add_output_labels()
+
+    def get_layer(self, size):
+        layer = VGroup()
+        n_neurons = size
+        if n_neurons > self.max_shown_neurons:
+            n_neurons = self.max_shown_neurons
+        neurons = VGroup(*[
+            Circle(
+                radius=self.neuron_radius,
+                stroke_color=self.neuron_stroke_color,
+                stroke_width=self.neuron_stroke_width,
+                fill_color=self.neuron_fill_color,
+                fill_opacity=0,
+            )
+            for x in range(n_neurons)
+        ])
+        neurons.arrange_submobjects(
+            DOWN, buff=self.neuron_to_neuron_buff
+        )
+        for neuron in neurons:
+            neuron.edges_in = VGroup()
+            neuron.edges_out = VGroup()
+        layer.neurons = neurons
+        layer.add(neurons)
+
+        if size > n_neurons:
+            dots = TexMobject("\\vdots")
+            dots.move_to(neurons)
+            VGroup(*neurons[:len(neurons) // 2]).next_to(
+                dots, UP, MED_SMALL_BUFF
+            )
+            VGroup(*neurons[len(neurons) // 2:]).next_to(
+                dots, DOWN, MED_SMALL_BUFF
+            )
+            layer.dots = dots
+            layer.add(dots)
+            if self.brace_for_large_layers:
+                brace = Brace(layer, LEFT)
+                brace_label = brace.get_tex(str(size))
+                layer.brace = brace
+                layer.brace_label = brace_label
+                layer.add(brace, brace_label)
+
+        return layer
+
+    def add_edges(self):
+        self.edge_groups = VGroup()
+        for l1, l2 in zip(self.layers[:-1], self.layers[1:]):
+            edge_group = VGroup()
+            for n1, n2 in it.product(l1.neurons, l2.neurons):
+                edge = self.get_edge(n1, n2)
+                edge_group.add(edge)
+                n1.edges_out.add(edge)
+                n2.edges_in.add(edge)
+            self.edge_groups.add(edge_group)
+        self.add_to_back(self.edge_groups)
+
+    def get_edge(self, neuron1, neuron2):
+        return Line(
+            neuron1.get_center(),
+            neuron2.get_center(),
+            buff=self.neuron_radius,
+            stroke_color=self.edge_color,
+            stroke_width=self.edge_stroke_width,
+        )
+
+
