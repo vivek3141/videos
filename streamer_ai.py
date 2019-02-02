e432d34aa333bf9e71df71dfab89be689265d718
Vivek Verma <vivnps.verma@gmail.com>
2019-02-02 00:29:59 -0800

    Adding Networks

--- streamer_ai.py
+++ streamer_ai.py
@@ -1,4 +1,5 @@
 from manim import *
+from files.network import *
 
 
 class Intro(Scene):
@@ -23,11 +24,13 @@ class InputANN(Scene):
         nn = SVGMobject("files/nn.svg")
         nn.move_to(2.5 * RIGHT)
 
-        head = TextMobject("Artificial Neural Network", c=BLUE)
+        head = TextMobject("Artificial Neural Network", color=BLUE)
         head.move_to(2 * UP)
+        head.scale(1.5)
 
-        foot = TextMobject("Recommender System")
+        foot = TextMobject("Recommender System", color=RED)
         foot.move_to(2 * DOWN)
+        foot.scale(1.5)
 
         self.play(ShowCreation(mario))
         self.play(ApplyMethod(mario.shift, 2.5 * LEFT), ShowCreation(a))
@@ -38,4 +41,46 @@ class InputANN(Scene):
         self.wait(2)
 
         self.play(ShowCreation(foot))
+        self.wait(2)
+
+
+class NeuralNetwork(Scene):
+    def construct(self):
+        network = NeuralNetworkMobject([3, 4, 3])
+        network.scale(2.5)
+
+        neuron_arrow = Arrow(ORIGIN, 1 * UP)
+        neuron_arrow.next_to(network, 1 * UP)
+
+        neuron_text = TextMobject("Neurons")
+        neuron_text.next_to(neuron_arrow, 1 * UP)
+
+        input_arrow = Arrow(1 * LEFT, ORIGIN, color=BLUE)
+        input_arrow.next_to(network, 2 * LEFT)
+
+        input_text = TextMobject("Inputs", color=BLUE)
+        input_text.next_to(input_arrow, 1 * UP)
+
+        output_arrow = Arrow(ORIGIN, 1 * RIGHT, color=RED)
+        output_arrow.next_to(network, 2 * RIGHT)
+
+        output_text = TextMobject("Outputs", color=RED)
+        output_text.next_to(output_arrow, 1 * UP)
+
+        weight_arrow = Arrow(ORIGIN, 1 * UP)
+        weight_arrow.next_to(network, 1 * UP)
+
+        weight_text = TextMobject("Weights")
+        weight_text.next_to(weight_arrow, 1 * UP)
+
+        self.play(ShowCreation(network))
+        self.wait(2)
+
+        self.play(ShowCreation(neuron_arrow), ShowCreation(neuron_text))
+        self.wait(2)
+
+        self.play(ShowCreation(input_arrow), ShowCreation(input_text))
+        self.wait(2)
+
+        self.play(ShowCreation(output_arrow), ShowCreation(output_text))
         self.wait(2)
