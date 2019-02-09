1a1090fb5f5958be3a3d1165eca8332100ee0ddb
Vivek Verma <vivnps.verma@gmail.com>
2019-02-08 17:28:17 -0800

    Adding Scalar Fields

---
+++ line_integral.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+python3 extract_scene.py line_integral.py Intro
+python3 extract_scene.py line_integral.py ScalarField
+python3 extract_scene.py line_integral.py Equation
+python3 extract_scene.py line_integral.py LineIntegralScalar
