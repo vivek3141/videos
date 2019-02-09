ba5d19f3ff7b3a9749c548b0def3b74c86015d7b
Vivek Verma <vivnps.verma@gmail.com>
2019-02-08 16:36:39 -0800

    Updating LineIntegral

--- line_integral.py
+++ line_integral.py
@@ -1,7 +1,12 @@
 from manim import *
 
 
-class LineIntegral(ThreeDScene):
+class Intro(Scene):
+    def construct(self):
+        pass
+
+
+class LineIntegralScalar(ThreeDScene):
 
     def construct(self):
         t_config = {
@@ -40,22 +45,16 @@ class LineIntegral(ThreeDScene):
 
         )
 
-        plane = ParametricSurface(
-            self.plane,
-            resulution=(16, 24),
-            u_min=-1,
-            u_max=1,
-            v_min=-1,
-            v_max=1,
-            fill_color=RED,
-            checkerboard_colors=[RED, RED]
-
-        )
-
         curve = ParametricFunction(
             self.c,
             t_min=-1,
             t_max=1,
+        )
+
+        curve_p = ParametricFunction(
+            self.c_project,
+            t_min=-1,
+            t_max=1
         )
 
         surface.scale(2)
@@ -69,37 +68,38 @@ class LineIntegral(ThreeDScene):
         text.scale(1.5)
         text.move_to(curve, 4 * RIGHT + 3 * DOWN)
 
-        #self.play(Write(integral))
+        area = self.get_riemann_sums(self.c_project, dx=0.005)
 
-        #self.play(ApplyMethod(integral.scale, 0.5))
-        #self.play(ApplyMethod(integral.shift, 3 * UP))
+        self.play(Write(integral))
+
+        self.play(ApplyMethod(integral.scale, 0.5))
+        self.play(ApplyMethod(integral.shift, 3 * UP))
 
         self.play(ShowCreation(axes))
-        # self.play(ShowCreation(surface))
-        # self.play(ShowCreation(curve), Write(text))
-        # self.play(Uncreate(integral), Uncreate(text))
-        # self.move_camera(0.8 * np.pi / 2, -0.45 * np.pi)
-        #
-        # self.begin_ambient_camera_rotation()
-        # self.wait(6)
-        #
-        # self.stop_ambient_camera_rotation()
-        self.move_camera(np.pi/2, -np.pi/2)
+        self.play(ShowCreation(surface))
+        self.play(ShowCreation(curve), Write(text))
+        self.play(Uncreate(integral), Uncreate(text))
+        self.move_camera(0.8 * np.pi / 2, -0.45 * np.pi)
 
-        self.wait()
-        #self.play(Uncreate(surface))
-        self.play(ShowCreation(plane), ShowCreation(curve))
+        self.begin_ambient_camera_rotation()
+        self.wait(6)
+
+        self.stop_ambient_camera_rotation()
+        self.move_camera(np.pi / 2, -np.pi / 2)
+        self.play(Transform(curve, curve_p), Uncreate(surface))
+        self.play(ShowCreation(area), ShowCreation(curve_p))
+
         self.wait(3)
 
-        # self.move_camera(0, -np.pi/2)
-        # self.play(Uncreate(axes), Uncreate(curve))
-        #
-        # self.play(Write(integral2))
-        #
-        # self.wait()
-        #
-        # self.play(Transform(integral2, sol))
-        # self.wait(6)
+        self.move_camera(0, -np.pi / 2)
+        self.play(Uncreate(axes), Uncreate(curve), Uncreate(area), Uncreate(curve_p))
+
+        self.play(Write(integral2))
+
+        self.wait()
+
+        self.play(Transform(integral2, sol))
+        self.wait(6)
 
     @staticmethod
     def func(x, y):
@@ -116,13 +116,22 @@ class LineIntegral(ThreeDScene):
             self.func(t, t ** 3)[-1]
         ])
 
-    def plane(self, x, y):
+    def c_project(self, t):
         return np.array([
-            x,
+            t,
             0,
-            self.func(x, y)[-1]
+            self.func(t, t ** 3)[-1]
         ])
 
+    @staticmethod
+    def get_riemann_sums(func, dx=0.2, x=(-1, 1), color=RED):
+        rects = VGroup()
+        for i in np.arange(x[0], x[1], dx):
+            h = func(i)[-1]
+            rect = Rectangle(height=h, width=dx, color=color)
+            rect.shift(i * RIGHT + (h / 2) * OUT)
+            rect.rotate(np.pi / 2, axis=X_AXIS)
+            rect.rotate(-np.pi / 2, axis=Z_AXIS)
+            rects.add(rect)
 
-
-
+        return rects
