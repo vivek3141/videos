5728441b4003ab8f8bc5f49e980793de385224e2
Vivek Verma <vivnps.verma@gmail.com>
2019-02-23 22:37:01 -0800

    Pacman AI Q Learning

--- pacman_ai.py
+++ pacman_ai.py
@@ -18,11 +18,37 @@ class QLearning(Scene):
         env = gym.make("Taxi-v2")
         self.q = np.zeros([env.observation_space.n, env.action_space.n])
         self.env = env
-        table = VGroup(*[
-            Line(3 * UP + i * RIGHT, 3 * DOWN + i * RIGHT) for i in range(-2, 3)
+        rows = VGroup(*[
+            Line(3 * UP + i * RIGHT, 3 * DOWN + i * RIGHT) for i in range(-4, 6, 2)
         ])
 
-        self.play(Write(table))
+        r = TextMobject("R")
+        r.shift(3 * LEFT + (3-(6/10)) * UP)
+        r.scale(1.5)
+
+        g = TextMobject("G")
+        g.shift(3 * RIGHT + (3 - (6 / 10)) * UP)
+        g.scale(1.5)
+
+        b = TextMobject("B")
+        b.shift(3 * LEFT + (3 - (6 / 10)) * DOWN)
+        b.scale(1.5)
+
+        y = TextMobject("Y")
+        y.shift(1 * RIGHT + (3 - (6 / 10)) * DOWN)
+        y.scale(1.5)
+
+        columns = VGroup(*[
+            Line(i * UP - 4 * RIGHT, i * UP + 4 * RIGHT) for i in np.arange(-3, 4, 6/5)
+        ])
+
+        self.play(Write(rows))
+        self.play(Write(columns))
+
+        self.wait()
+
+        self.play(Write(r), Write(g), Write(y), Write(b))
+        self.wait()
 
     def continual_update(self, dt):
         pass
