cd7d677c6e58c4099aab6718103c94c91e5c6b67
Vivek Verma <vivnps.verma@gmail.com>
2019-06-08 16:51:15 -0700

    Green Theorem

--- green_theorem.py
+++ green_theorem.py
@@ -95,29 +95,124 @@ class Intro(Scene):
     def construct(self):
         eq = TexMobject(
             r"\int_C \vec{\text{F}} \bullet \text{d}\vec{\text{r}} = \iint_D \nabla \times \vec{\text{F}} \ \text{dA}")
-        eq.scale(1.5)
-        
+        eq.scale(1)
+
         title = TextMobject("Line Integral")
-        title.scale(1.5)
+        title.scale(1)
         title.to_edge(UP)
-        rect = ScreenRectangle(height=6)
+        rect = ScreenRectangle(height=4)
         rect.next_to(title, DOWN)
+
+        title.shift(2 * DOWN)
+        rect.shift(2 * DOWN)
 
         self.play(Write(eq))
         self.wait()
 
         self.play(ApplyMethod(eq.shift, 3 * UP))
         self.wait()
-        
+
         self.play(
             FadeInFromDown(title),
             Write(rect)
         )
+        self.wait()
 
 
-class Diagram(Scene):
+class Setup(Scene):
+    CONFIG = {
+        "color_list": ['#e22b2b', '#e88e10', '#eae600', '#88ea00',
+                       '#00eae2', '#0094ea', "#2700ea", '#bf00ea', '#ea0078'],
+        "prop": 0
+    }
+
     def construct(self):
-        pass
+        axes_config = {"x_min": -5,
+                       "x_max": 5,
+                       "y_min": -5,
+                       "y_max": 5,
+                       "z_axis_config": {},
+                       "z_min": -1,
+                       "z_max": 1,
+                       "z_normal": DOWN,
+                       "num_axis_pieces": 20,
+                       "light_source": 9 * DOWN + 7 * LEFT + 10 * OUT,
+                       "number_line_config": {
+                           "include_tip": False,
+                       },
+                       }
+
+        axes = Axes(**axes_config)
+        f = VGroup(
+            *[self.calc_field_color(x * RIGHT + y * UP, self.vect, prop=0)
+              for x in np.arange(-5, 5, 1)
+              for y in np.arange(-5, 5, 1)
+              ]
+        )
+
+        field = VGroup(axes, f)
+
+        c = ParametricFunction(
+            self.func,
+            t_min=-2,
+            t_max=2,
+        )
+        c.set_stroke(opacity=0.75)
+        label = TextMobject("C")
+        label.shift(3 * LEFT)
+        label.scale(2)
+
+        s = ParametricSurface(
+            self.surface,
+            u_min=-2,
+            u_max=2,
+            v_min=-1,
+            v_max=1,
+            fill_color=BLUE,
+            checkerboard_colors=[BLUE, BLUE],
+            stroke_color=BLUE
+        ).set_fill(opacity=0.5)
+
+        r = TextMobject("R", color=BLUE)
+        r.scale(1.5)
+        r.shift(2 * RIGHT + 3.5 * UP)
+
+        curve = VGroup(label, c)
+        surface = VGroup(r, s)
+
+        self.play(ShowCreation(field))
+        self.wait()
+
+        self.play(Write(curve))
+        self.wait()
+
+        self.play(Transform(field, field2))
+        self.wait()
+
+        self.play(Write(surface))
+        self.wait()
+
+    def calc_field_color(self, point, f, prop=0.0, opacity=None):
+        x, y = point[:2]
+        func = f(x, y)
+        magnitude = math.sqrt(func[0] ** 2 + func[1] ** 2)
+        func = func / magnitude if magnitude != 0 else np.array([0, 0])
+        func = func / 1.5
+        v = int(magnitude / 10 ** prop)
+        index = len(self.color_list) - 1 if v > len(self.color_list) - 1 else v
+        c = self.color_list[index]
+        v = Vector(func, color=c).shift(point)
+        if opacity:
+            v.set_fill(opacity=opacity)
+        return v
+     @staticmethod
+    def vect(x, y):
+        return np.array([
+            y,
+            x,
+            0
+        ])
+
 
 
 class GreenTheoremVisual(Scene):
@@ -152,18 +247,6 @@ class GreenTheoremVisual(Scene):
         )
 
         field = VGroup(axes, f)
-        # field.scale(0.6)
-
-        axes2 = Axes(**axes_config)
-        f2 = VGroup(
-            *[self.calc_field_color(x * RIGHT + y * UP, self.vect, prop=0).set_fill(opacity=0.3)
-              for x in np.arange(-5, 5, 1)
-              for y in np.arange(-5, 5, 1)
-              ]
-        )
-
-        field2 = VGroup(axes, f2)
-        # field2.scale(0.6)
 
         c = ParametricFunction(
             self.func,
