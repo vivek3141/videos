6633c01082c0f37c13e6a51c04547dbaa18ca202
Vivek Verma <vivnps.verma@gmail.com>
2019-06-16 19:44:30 -0700

    Gaussian Integral

--- gaussian.py
+++ gaussian.py
@@ -4,3 +4,19 @@ from manimlib.imports import *
 class Intro(Scene):
     def construct(self):
         pass
+
+
+class GaussianVisual(ThreeDScene):
+    def construct(self):
+        surface = ParametricSurface(
+            self.func,
+        )
+        self.move_camera(0.8 * np.pi / 2, -0.45 * np.pi)
+        self.play(Write(surface))
+
+    def func(self, u, v):
+        return np.array([
+            u,
+            v,
+            np.exp(-(u**2 + v**2))
+        ])
