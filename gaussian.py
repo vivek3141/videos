c2b18e5511f3d737c0fa2d87828b98c87377ce10
Vivek Verma <vivnps.verma@gmail.com>
2019-06-16 19:46:48 -0700

    Gaussian Integral

--- gaussian.py
+++ gaussian.py
@@ -10,7 +10,11 @@ class GaussianVisual(ThreeDScene):
     def construct(self):
         surface = ParametricSurface(
             self.func,
-        )
+            u_min=-2,
+            u_max=2,
+            v_min=-2
+            v_max=2
+        ).scale(2)
         self.move_camera(0.8 * np.pi / 2, -0.45 * np.pi)
         self.play(Write(surface))
 
