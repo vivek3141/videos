1b48aac29789e07a18ed6c1f16eb63c0dbc113a0
Vivek Verma <vivnps.verma@gmail.com>
2019-06-16 19:54:22 -0700

    Gaussian Integral

--- gaussian.py
+++ gaussian.py
@@ -3,7 +3,18 @@ from manimlib.imports import *
 
 class Intro(Scene):
     def construct(self):
-        pass
+        f = ParametricFunction(
+            function=lambda t: np.array([t, np.exp(-t**2), 0])
+        )
+        axes = Axes(
+            x_min=-3,
+            x_max=3,
+            y_min=0,
+            y_max=1.5
+        )
+        func = VGroup(f, axes)
+
+        self.play(Write(func))
 
 
 class GaussianVisual(ThreeDScene):
@@ -12,11 +23,12 @@ class GaussianVisual(ThreeDScene):
             self.func,
             u_min=-2,
             u_max=2,
-            v_min=-2
+            v_min=-2,
             v_max=2
         ).scale(2)
         self.move_camera(0.8 * np.pi / 2, -0.45 * np.pi)
         self.play(Write(surface))
+        self.wait()
 
     def func(self, u, v):
         return np.array([
