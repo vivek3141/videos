fb64f28d1e7c9a5225ab024c47e3c8956126e904
Vivek Verma <vivnps.verma@gmail.com>
2019-07-03 19:20:06 -0700

    Gaussian Integral

--- gaussian.py
+++ gaussian.py
@@ -686,3 +686,51 @@ class Gauss(Scene):
         eq = TextMobject(r"Carl Friedrich Gauss").scale(1.5).shift(2.5 * DOWN)
         self.play(Write(eq))
         self.wait()
+
+
+class Thumb(Scene):
+
+    def construct(self):
+        f = ParametricFunction(
+            function=self.func,
+            t_min=-3,
+            t_max=3,
+            color=BLUE
+        )
+
+        axes = Axes(
+            x_min=-3,
+            x_max=3,
+            y_min=0,
+            y_max=2,
+            number_line_config={
+                "color": LIGHT_GREY,
+                "include_tip": False,
+                "exclude_zero_from_default_numbers": True,
+            }
+        )
+
+        rect = self.get_riemann_sums(self.func)
+        rect.scale(1.95)
+        rect.shift(2.4 * DOWN)
+
+        func = VGroup(axes, f)
+        func.scale(2)
+
+        self.play(Write(func))
+        self.play(Write(rect))
+
+    @staticmethod
+    def get_riemann_sums(func, dx=0.01, x=(-3, 3), color=RED):
+        rects = VGroup()
+        for i in np.arange(x[0], x[1], dx):
+            h = func(i)[1]
+            rect = Rectangle(height=h, width=dx, color=color,
+                             stroke_opacity=0.3, fill_opacity=0.3)
+            rect.shift(i * RIGHT + (h / 2) * UP)
+            rects.add(rect)
+
+        return rects
+
+    def func(self, t):
+        return np.array([t, np.exp(-t**2), 0])
