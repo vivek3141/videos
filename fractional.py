3f93a8cdebc5280954b1a700d67819a02e272a4f
Vivek Verma <vivnps.verma@gmail.com>
2019-07-11 14:50:14 -0700

    Fractional Calculus

--- fractional.py
+++ fractional.py
@@ -1,6 +1,30 @@
-from manimlib.improts import *
+from manimlib.imports import *
 
 
 class Intro(Scene):
     def construct(self):
-        pass
+        f = ParametricFunction(
+            func,
+            t_min=0,
+            t_max=1,
+            color=GREEN
+        )
+        axes = Axes(
+            x_min=-1,
+            x_max=3,
+            y_min=0,
+            y_max=4,
+            number_line_config={
+                "include_tip": False,
+            }
+        )
+
+        func = VGroup(axes, f)
+        self.play(Write(func))
+
+    def func(t):
+        return np.array([
+            t,
+            t**4 - 2*t**3 + t + 1,
+            0
+        ])
