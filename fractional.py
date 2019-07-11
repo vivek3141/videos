77435c44ee74be359fae73c11bfd94bbad522a47
Vivek Verma <vivnps.verma@gmail.com>
2019-07-11 14:53:44 -0700

    Fractional Calculus

--- fractional.py
+++ fractional.py
@@ -6,7 +6,7 @@ class Intro(Scene):
         f = ParametricFunction(
             self.func,
             t_min=0,
-            t_max=2,
+            t_max=1.75,
             color=GREEN
         )
         axes = Axes(
@@ -20,7 +20,25 @@ class Intro(Scene):
         )
 
         func = VGroup(axes, f)
+        func.scale(2.5)
+        func.shift(2 * LEFT)
+
+        eq1 = TexMobject(r"\frac{dy}{dx}")
+        eq1.scale(1.5)
+        eq1.shift(2 * RIGHT)
+
+        eq2 = TexMobject(r"\frac{d^2y}{dx^2}")
+        eq2.scale(1.5)
+        eq2.shift(2 * RIGHT)
+
         self.play(Write(func))
+        self.wait()
+
+        self.play(Write(eq1))
+        self.wait()
+
+        self.play(Transform(eq1, eq2))
+        self.wait()
 
     @staticmethod
     def func(t):
