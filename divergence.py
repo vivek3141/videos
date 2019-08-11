0b3ce9aa78f4e8251022348283bfaf6f45b35843
Vivek Verma <vivnps.verma@gmail.com>
2019-08-10 20:39:44 -0700

    Divergence Theorem

--- divergence.py
+++ divergence.py
@@ -352,12 +352,79 @@ class FluxIntegral(Scene):
             0
         ])
 
+
 class FluxExample(Scene):
     CONFIG = {
         "color_list": ['#e22b2b', '#e88e10', '#eae600', '#88ea00',
                        '#00eae2', '#0094ea', "#2700ea", '#bf00ea', '#ea0078'],
         "prop": 0
     }
+
+    def construct(self):
+        axes = Axes(
+            x_min=-5,
+            x_max=5,
+            y_min=-5,
+            y_max=5,
+            number_line_config={"include_tip": False, }
+        )
+        f = VGroup(
+            *[self.calc_field_color(x * RIGHT + y * UP, self.vect, prop=0)
+              for x in np.arange(-5, 6, 1)
+              for y in np.arange(-5, 6, 1)
+              ]
+        )
+
+        field = VGroup(axes, f)
+
+        c = ParametricFunction(
+            self.func,
+            t_min=0,
+            t_max=2*PI,
+            stroke_width=1.5 * DEFAULT_STROKE_WIDTH,
+        )
+
+        curve = c
+
+        field.set_fill(opacity=0.75)
+        field.set_stroke(opacity=0.75)
+
+        self.play(ShowCreation(field))
+        self.wait()
+
+        self.play(Write(curve))
+        self.wait()
+
+    def calc_field_color(self, point, f, prop=0.0, opacity=None):
+        x, y = point[:2]
+        func = f(x, y)
+        magnitude = math.sqrt(func[0] ** 2 + func[1] ** 2)
+        func = func / magnitude if magnitude != 0 else np.array([0, 0])
+        func = func / 1.5
+        v = int(magnitude / 10 ** prop)
+        index = len(self.color_list) - 1 if v > len(self.color_list) - 1 else v
+        c = self.color_list[index]
+        v = Vector(func, color=c).shift(point)
+        if opacity:
+            v.set_fill(opacity=opacity)
+        return v
+
+    @staticmethod
+    def vect(x, y):
+        return np.array([
+            x*y+x,
+            x+y,
+            0
+        ])
+
+    @staticmethod
+    def func(t):
+        return np.array([
+            math.cos(t),
+            math.sin(t),
+            0
+        ])
+
 
 class Setup(Scene):
     CONFIG = {
@@ -422,7 +489,27 @@ class Setup(Scene):
         func = func / 1.5
         v = int(magnitude / 10 ** prop)
         index = len(self.color_list) - 1 if v > len(self.color_list) - 1 else v
-        k
+        c = self.color_list[index]
+        v = Vector(func, color=c).shift(point)
+        if opacity:
+            v.set_fill(opacity=opacity)
+        return v
+
+    @staticmethod
+    def vect(x, y):
+        return np.array([
+            x*y+x,
+            x+y,
+            0
+        ])
+
+    @staticmethod
+    def func(t):
+        return np.array([
+            math.cos(t),
+            math.sin(t),
+            0
+        ])
 
     @staticmethod
     def surface(t, v):
