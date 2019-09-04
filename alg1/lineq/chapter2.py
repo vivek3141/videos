1651702e6847d38e3a5d94004e9ed540893ccb59
Vivek Verma <vivnps.verma@gmail.com>
2019-09-04 11:11:13 -0700

    Lineq Chapter 2

--- alg1/lineq/chapter2.py
+++ alg1/lineq/chapter2.py
@@ -14,3 +14,19 @@ class Intro(Scene):
 
         self.play(Uncreate(title), Uncreate(rect))
         self.wait()
+
+        axes = Axes(
+            x_min=-5,
+            x_max=5,
+            y_min=-4,
+            y_max=4,
+            number_line_config={
+                "include_tip": False,
+            }
+        )
+
+        f1 = FunctionGraph(lambda x: x)
+        f2 = FunctionGraph(lambda x: 3*x, color=RED)
+
+        self.play(Write(axes), Write(f1), Write(f2))
+        self.wait()
