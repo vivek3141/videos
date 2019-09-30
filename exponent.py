ab9c6e8d7ce4a592712f3ea3d50b5cd2f22badb4
Vivek Verma <vivnps.verma@gmail.com>
2019-09-30 10:46:37 -0700

    Exponents

--- exponent.py
+++ exponent.py
@@ -7,7 +7,7 @@ class ECircle(Scene):
         plane.add(plane.get_axis_labels())
 
         curve = ParametricFunction(
-            function=lambda t: np.array(2*[np.cos(t), np.sin(t), 0])
+            function=lambda t: np.array(2*[np.cos(t), np.sin(t), 0]),
             t_min=0,
             t_max=2*PI,
             color=YELLOW
