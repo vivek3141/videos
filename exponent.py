9d19a004f2adfc4196df8aea8e134060c1c01332
Vivek Verma <vivnps.verma@gmail.com>
2019-09-30 10:46:05 -0700

    Exponents

---
+++ exponent.py
@@ -0,0 +1,15 @@
+from manimlib.imports import *
+
+
+class ECircle(Scene):
+    def construct(self):
+        plane = NumberPlane()
+        plane.add(plane.get_axis_labels())
+        curve = ParametricFunction(
+            function=lambda t: np.array(2*[np.cos(t), np.sin(t), 0])
+            t_min=0,
+            t_max=2*PI,
+            color=YELLOW
+        )
+
+        self.add(plane)
