d413866de0fa332636a3218ca626d83837db2b15
Vivek Verma <vivnps.verma@gmail.com>
2019-10-12 23:17:05 -0700

    Gabriel's Horn

--- gabriel.py
+++ gabriel.py
@@ -9,7 +9,15 @@ class Horn(ThreeDScene):
     def construct(self):
         surface = ParametricSurface(
             self.func,
+            u_min=0.01,
+            u_max=5,
+            v_min=0,
+            v_max=2*PI
         )
+
+        self.move_camera(0.8 * np.pi / 2, -0.45 * np.pi)
+        self.play(Write(surface))
+        self.wait()
 
     @staticmethod
     def func(u, v):
