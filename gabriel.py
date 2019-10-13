6679edf4a354310585ca9d9fc52d5b7482a3d8ed
Vivek Verma <vivnps.verma@gmail.com>
2019-10-12 23:13:59 -0700

    Gabriel's Horn

--- gabriel.py
+++ gabriel.py
@@ -3,3 +3,17 @@ from manimlib.imports import *
 
 class Intro(Scene):
     pass
+
+
+class Horn(ThreeDScene):
+    def construct(self):
+        surface = ParametricSurface(
+            self.func, 
+        )
+    @staticmethod
+    def func(u, v):
+        return np.array([
+            u,
+            (1/u)*np.cos(v),
+            (1/u)*np.sin(v)
+        ])
