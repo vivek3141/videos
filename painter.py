5dc1e0608fcb2f3f896c68089b34629ecede8beb
Vivek Verma <vivnps.verma@gmail.com>
2019-11-06 01:00:28 -0800

    Painter's Paradox

--- painter.py
+++ painter.py
@@ -3,7 +3,10 @@ from manimlib.imports import *
 
 class Intro(Scene):
     def construct(self):
-        pass
+        fig = PiCreature()
+
+        self.play(Write(fig))
+        self.wait()
 
 
 class Revolution(ThreeDScene):
@@ -484,3 +487,102 @@ class Horn(ThreeDScene):
             (1/u)*np.cos(v),
             (1/u)*np.sin(v)
         ])
+
+class BannerE(Scene):
+    CONFIG = {
+        "color_list": ['#e22b2b', '#e88e10', '#eae600', '#88ea00',
+                       '#00eae2', '#0094ea', "#2700ea", '#bf00ea', '#ea0078'],
+        "prop": 0
+    }
+
+    def construct(self):
+
+        axes_config = {"x_min": -5,
+                       "x_max": 5,
+                       "y_min": -5,
+                       "y_max": 5,
+                       "z_axis_config": {},
+                       "z_min": -1,
+                       "z_max": 1,
+                       "z_normal": DOWN,
+                       "num_axis_pieces": 20,
+                       "light_source": 9 * DOWN + 7 * LEFT + 10 * OUT,
+                       "number_line_config": {
+                           "include_tip": False,
+                       },
+                       }
+
+        axes = Axes(**axes_config)
+        f = VGroup(
+            *[self.calc_field_color(x * RIGHT + y * UP, lambda x, y: np.array([y, x]), prop=0)
+              for x in np.arange(-5, 6, 1)
+              for y in np.arange(-5, 6, 1)
+              ]
+        )
+
+        field = VGroup(axes, f)
+
+        c = ParametricFunction(
+            self.func,
+            t_min=-3,
+            t_max=3,
+        )
+        c.set_stroke(opacity=0.75)
+        label = TextMobject("C")
+        label.shift(2 * LEFT)
+        label.scale(2)
+
+        curve = VGroup(label, c)
+
+        everything = VGroup(field, curve)
+        everything.scale(0.7)
+
+        self.play(Write(everything))
+        self.wait()
+
+    def calc_field_color(self, point, f, prop=0.0, opacity=None):
+        x, y = point[:2]
+        func = f(x, y)
+        magnitude = math.sqrt(func[0] ** 2 + func[1] ** 2)
+        func = func / magnitude if magnitude != 0 else np.array([0, 0])
+        func = func / 1.5
+        v = int(magnitude / 10 ** prop)
+        index = len(self.color_list) - 1 if v > len(self.color_list) - 1 else v
+        c = self.color_list[index]
+        v = Vector(func, color=c).shift(point)
+        if opacity:
+            v.set_fill(opacity=opacity)
+        return v
+
+    @staticmethod
+    def func(t):
+        return np.array([
+            2 * np.arctan(t),
+            t,
+            0
+        ])
+
+    def f_r(self, t):
+        r = self.func(t)
+        return np.array([
+            r[1],
+            r[0],
+            0
+        ])
+
+    @staticmethod
+    def line_evaluated(x):
+        r_derivative = np.array([2 / (1 + x ** 2), 1])
+        f_r = np.array([x, 2 * np.arctan(x)])
+
+        return np.array([
+            x,
+            0.25 * np.dot(f_r, r_derivative),
+            0
+        ])
+
+    @staticmethod
+    def r_prime(x):
+        r_d = np.array([2 / (1 + x ** 2), 1, 0])
+        return r_d
+
