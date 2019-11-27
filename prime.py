12923aaf6d8957740c4c4bc14d82b1da581599ac
Vivek Verma <vivnps.verma@gmail.com>
2019-11-26 23:02:57 -0800

    Prime Distribution

--- prime.py
+++ prime.py
@@ -46,15 +46,15 @@ class PrimeFuncGraph(GraphScene):
 
 class PNTGraph(GraphScene):
     CONFIG = {
-        "y_max": 10,
+        "y_max": 2,
         "y_min": 0,
-        "x_max": 3000,
-        "x_min": 0,
+        "x_max": 1000,
+        "x_min": 0.001,
         "y_tick_frequency": 1,
         "x_tick_frequency": 100,
         "axes_color": BLUE,
         "x_axis_label": "$x$",
-        "y_axis_label": "$\pi(x)$",
+        "y_axis_label": r"$\frac{\pi(x)}{x/ \ln (x)}$",
         "g_color": RED,
         "g_width": DEFAULT_STROKE_WIDTH*1,
     }
@@ -63,7 +63,7 @@ class PNTGraph(GraphScene):
         self.setup_axes()
         f1 = self.get_graph(
             self.pnt,
-            color=PINK,
+            color=RED,
         )
 
         self.play(Write(f1))
