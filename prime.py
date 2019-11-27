d3209f66281b7f4bb4c719fd45a7b3bfc4ad6b45
Vivek Verma <vivnps.verma@gmail.com>
2019-11-26 22:50:22 -0800

    Prime Distribution

--- prime.py
+++ prime.py
@@ -27,4 +27,18 @@ class PrimeFuncGraph(GraphScene):
 
     @staticmethod
     def count_prime(x):
-        return x
+        x = int(x)
+        counter = 0
+
+        for i in range(x):
+            if isPrime(i):
+                counter += 1
+
+        return counter
+
+    @staticmethod
+    def isPrime(x):
+        for i in range(2, int((x+1)/2)):
+            if x % i == 0:
+                return False
+        return True
