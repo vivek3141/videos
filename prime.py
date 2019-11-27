aa53b12b485e06a5e6102b66c59e9d9e04f9bc5e
Vivek Verma <vivnps.verma@gmail.com>
2019-11-26 22:46:56 -0800

    Prime Distribution

--- prime.py
+++ prime.py
@@ -2,14 +2,14 @@ from manimlib.imports import *
 
 class PrimeFuncGraph(GraphScene):
     CONFIG = {
-        "y_max": 3,
+        "y_max": 100,
         "y_min": 0,
-        "x_max": 4,
+        "x_max": 20,
         "x_min": 0,
         "y_tick_frequency": 5,
         "axes_color": BLUE,
-        "x_axis_label": "$t$",
-        "y_axis_label": "$f(t)$",
+        "x_axis_label": "$x$",
+        "y_axis_label": "$\pi(x)$",
         "g_color": RED,
         "g_width": DEFAULT_STROKE_WIDTH*1,
     }
@@ -20,3 +20,8 @@ class PrimeFuncGraph(GraphScene):
             lambda t:  0.5*t**2,
             color=PINK,
         )
+
+        self.play(Write(f1))
+        self.wait()
+    
+    
