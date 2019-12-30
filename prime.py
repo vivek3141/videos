462b32315b785fc23eb4bcf056788eae8407d1f4
Vivek Verma <vivnps.verma@gmail.com>
2019-12-30 12:23:35 -0800

    Primes

--- prime.py
+++ prime.py
@@ -1,7 +1,6 @@
 from manimlib.imports import *
 from scipy.special import expi
 from scipy.integrate import quad
-import riemann
 from mpmath import *
 from mpmath.libmp.libintmath import moebius
 
@@ -42,7 +41,7 @@ class PrimeMethods:
 
     def _riemann_int(self, t):
         return 1/(t * (t**2 - 1)*math.log(t))
-    
+
     def j_zeros(self, x, zz):
         x = float(x)
         zz = [mpc(0.5, float(y)) for y in zz]
@@ -54,20 +53,18 @@ class PrimeMethods:
 
         return li(x) - summ + integral - log(2)
 
-
     def pi_zeros(self, x, zz):
         x = float(x)
         sup_lim = int(log(x)/log(2.0)) + 2
         summatory = mpf(0)
         for n in range(1, sup_lim+1):
             n = mpf(n)
-            jn = j_zeros(power(x, 1.0/n), zz)
+            jn = self.j_zeros(power(x, 1.0/n), zz)
             mu = moebius(n)
             summatory += mu*jn/n
         return summatory
 
-
-    def single_pi(x, num_zeros, zeros_file):
+    def single_pi(self, x, num_zeros, zeros_file):
         f = open(zeros_file)
         nzeros = []
         i = 1
@@ -76,13 +73,12 @@ class PrimeMethods:
                 break
             nzeros.append(mpf(l.strip()))
             i += 1
-        return pi_zeros(x, nzeros)
-
+        return self.pi_zeros(x, nzeros)
 
     def riemann_count(self, x, num_zeros=35):
         # s = sum([self.li(x**i) for i in self.zeros[0:num_zeros]])
         # return self.li(x) - s - math.log(2) + quad(self._riemann_int, x, np.inf)[0]
-        return float(riemann.single_pi(x, num_zeros, "zeros.txt"))
+        return float(self.single_pi(x, num_zeros, "zeros.txt"))
 
 
 class Test(GraphScene, PrimeMethods):
