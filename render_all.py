093aae3e46fe6c0cb99e74bd6935304dba4c4cd5
Vivek Verma <vivnps.verma@gmail.com>
2020-01-19 15:50:58 -0800

    Render all script

--- render_all.py
+++ render_all.py
@@ -2,7 +2,14 @@ import sys
 
 with open(sys.argv[1], "r") as f:
     code = f.read()
-    
+
+li = []
 ind = 0
 while(True):
     ind = code.find("class", start=ind)
+    if ind == -1:
+        break
+    li.append(code[ind+1:code.find(" ", start=ind+2)])
+
+print(li)
+
