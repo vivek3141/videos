a337128ee34e6cbece831331aeba53f10fe42557
Vivek Verma <vivnps.verma@gmail.com>
2020-02-29 18:34:11 -0800

    Maxwell's Equations

--- maxwell.py
+++ maxwell.py
@@ -81,22 +81,37 @@ class Equations(Scene):
 class EMScene(Scene):
     CONFIG = {
         "frequency": 1,
-        "num_vects": 40,
-        "alpha": 1.5,
+        "num_vects": 30,
+        "alpha": 1,
+        "run_time": 8,
         "osc_freq": 1
     }
 
     def construct(self):
+        self.tracker = ValueTracker(0)
         wave = VGroup(
-            *[self.get_vect(E_COLOR, t) for t in np.linspace(0, 4*PI, num=self.num_vects)]
-        ).center()
-
+            *[self.get_vect(E_COLOR, t) for t in np.linspace(-2*PI, 2*PI, num=self.num_vects)]
+        )
         self.play(Write(wave))
+        self.play(
+            self.tracker.increment_value,
+            self.osc_freq * self.run_time * PI,
+            run_time=self.run_time,
+            rate_func=linear
+        )
         self.wait()
 
     def get_vect(self, color, t):
-        length = self.alpha * np.sin(self.frequency * t)
-        return Vector(direction=np.array([0, length, 0]), color=color).shift(t * RIGHT)
+        length = self.alpha * np.sin(t)
+        vect = Vector(direction=np.array(
+            [0, length, 0]), color=color).shift(t * RIGHT)
+        vect.add_updater(lambda x: x.become(
+            self.get_vect_updater(self.tracker.get_value(), color, t)))
+        return vect
+
+    def get_vect_updater(self, t, color, phi):
+        length = self.alpha * np.sin(self.frequency * t + self.frequency * phi)
+        return Vector([0, length, 0], color=color).shift(phi * RIGHT)
 
 
 class TestWave(ThreeDScene):
