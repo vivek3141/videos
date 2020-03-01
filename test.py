e7dc7340a49877a793cbc447d65925c5a1832628
Vivek Verma <vivnps.verma@gmail.com>
2020-02-29 19:53:23 -0800

    Maxwell's Equations

--- test.py
+++ test.py
@@ -213,46 +213,6 @@ class EMWave(VGroup):
         return ThreeDVector(length * direction, color=color).shift(phi * RIGHT)
 
 
-class Wave(VGroup):
-    def __init__(
-        self,
-        E_COLOR=YELLOW, M_COLOR=BLUE,
-        frequency=1,
-        alpha=1,
-        num_vects=30,
-        start=-FRAME_WIDTH/2,
-        end=FRAME_WIDTH/2,
-        direction=IN,
-        **kwargs
-    ):
-        VGroup.__init__(self, **kwargs)
-        self.alpha = alpha
-        self.frequency = frequency
-        self.start = start
-        self.end = end
-        self.tracker = ValueTracker(0)
-        wave = VGroup(
-            *[self.get_vect(E_COLOR, t, direction=direction) for t in np.linspace(self.start, self.end, num=num_vects)]
-        )
-        self.add(wave)
-
-    def get_t(self, value):
-        return value - self.start
-
-    def get_vect(self, color, t, direction=IN):
-        x = self.get_t(t)
-        length = self.alpha * np.sin(x)
-        vect = VGroup(
-            Line(ORIGIN, direction * length, color=color).shift(t * RIGHT),
-        )
-        tip = ArrowTip(
-            start_angle=PI/2,
-            color=color).shift(direction * length).shift(t * RIGHT).rotate(axis=[1, 0, 0], angle=PI/2).rotate(axis=[1, 0, 0], angle=PI)
-        # if direction * length
-        vect.add(tip)
-        return vect
-
-
 class EMTest(ThreeDScene):
     def construct(self):
         self.move_camera(0.4 * np.pi / 2, -PI/2)
@@ -260,39 +220,3 @@ class EMTest(ThreeDScene):
         self.add(wave)
         self.play(wave.tracker.increment_value, 4 *
                   PI, run_time=4, rate_func=linear)
-
-
-class EMScene(Scene):
-    CONFIG = {
-        "frequency": 1,
-        "num_vects": 30,
-        "alpha": 1,
-        "run_time": 8,
-        "osc_freq": 1
-    }
-
-    def construct(self):
-        self.tracker = ValueTracker(0)
-        wave = VGroup(
-            *[self.get_vect(YELLOW, t) for t in np.linspace(-2*PI, 2*PI, num=self.num_vects)]
-        )
-        self.play(Write(wave))
-        self.play(
-            self.tracker.increment_value,
-            self.osc_freq * self.run_time * PI,
-            run_time=self.run_time,
-            rate_func=linear
-        )
-        self.wait()
-
-    def get_vect(self, color, t):
-        length = self.alpha * np.sin(t)
-        vect = Vector(direction=np.array(
-            [0, length, 0]), color=color).shift(t * RIGHT)
-        vect.add_updater(lambda x: x.become(
-            self.get_vect_updater(self.tracker.get_value(), color, t)))
-        return vect
-
-    def get_vect_updater(self, t, color, phi):
-        length = self.alpha * np.sin(self.frequency * t + self.frequency * phi)
-        return Vector([0, length, 0], color=color).shift(phi * RIGHT)
