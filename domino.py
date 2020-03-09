70785f0ada2fd5225b1894740b34a0debadb5ee8
Vivek Verma <vivnps.verma@gmail.com>
2020-03-09 13:04:59 -0700

    Dominos

--- domino.py
+++ domino.py
@@ -1,1 +1,21 @@
+from manimlib.imports import *
+
+
+
+class Table(VGroup):
+    def __init__(self, m, n, s_width=1, s_length=1):
+        for x in range(0, m):
+            for y in range(0, n):
+                self.add()
+
+
+class DominoScene(Scene):
+    CONFIG = {
+        "m": 4,
+        "n": 4,
+        "s_width": 1,
+        "s_length": 1
+    }
+    def construct(self):
+        self.table = VGroup()
 
