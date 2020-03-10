780accf753abaf53f7ed6f7e73f44801825417b9
Vivek Verma <vivnps.verma@gmail.com>
2020-03-10 14:09:08 -0700

    Dominos

--- domino.py
+++ domino.py
@@ -1,28 +1,29 @@
 from manimlib.imports import *
 
 
-
-class Table(VGroup):
+class Chessboard(VGroup):
     def __init__(self, m, n, s_width=1, s_length=1, **kwargs):
         VGroup.__init__(self, **kwargs)
         for x in range(0, m + 1):
             self.add(Line(
-                s_width * np.array([x - int(m/2), -int(n/2), 0]),
-                s_width * np.array([x - int(m/2), int(n/2), 0]))
+                s_width * np.array([x - (m/2), -(n/2), 0]),
+                s_width * np.array([x - (m/2), (n/2), 0]))
                 )
         for y in range(0, n + 1):
             self.add(Line(
-                s_length * np.array([-int(m/2), y - int(n/2), 0]),
-                s_length * np.array([int(m/2), y - int(n/2), 0]))
+                s_length * np.array([-(m/2), y - (n/2), 0]),
+                s_length * np.array([(m/2), y - (n/2), 0]))
                 )
+
+
 
 class DominoScene(Scene):
     CONFIG = {
-        "m": 4,
+        "m": 5,
         "n": 4,
-        "s_width": 2,
-        "s_length": 2
+        "s_width": 1.5,
+        "s_length": 1.5
     }
     def construct(self):
-        self.table = Table(self.m, self.n, s_width=self.s_width, s_length=self.s_length)
+        self.table = Chessboard(self.m, self.n, s_width=self.s_width, s_length=self.s_length)
         self.add(self.table)
