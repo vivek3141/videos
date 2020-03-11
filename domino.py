3786e387a5ec0613dfdb60e56d00cd864d9dcea6
Vivek Verma <vivnps.verma@gmail.com>
2020-03-11 00:14:42 -0700

    Domino

--- domino.py
+++ domino.py
@@ -6,20 +6,29 @@ class Chessboard(VGroup):
         VGroup.__init__(self, **kwargs)
         for x in range(0, m + 1):
             self.add(Line(
-                s_width * np.array([x - (m/2), -(n/2), 0]),
-                s_width * np.array([x - (m/2), (n/2), 0]))
+                s_width * np.array([x - m/2, -n/2, 0]),
+                s_width * np.array([x - m/2, n/2, 0]))
                 )
         for y in range(0, n + 1):
             self.add(Line(
-                s_length * np.array([-(m/2), y - (n/2), 0]),
-                s_length * np.array([(m/2), y - (n/2), 0]))
+                s_length * np.array([-m/2, y - n/2, 0]),
+                s_length * np.array([m/2, y - n/2, 0]))
                 )
 
+        for x in range(0, n + 1):
+            if m % 2 == 0: init = 0; end = m
+            else: init = 1; end = m + 1
+            for y in range(init, end):
+                self.add(Rectangle(
+                    width=s_width,
+                    length=s_length,
+                    opacity=1,
+                    color=RED).move_to([y - m/2, x - n/2, 0]))
 
 
 class DominoScene(Scene):
     CONFIG = {
-        "m": 5,
+        "m": 4,
         "n": 4,
         "s_width": 1.5,
         "s_length": 1.5
