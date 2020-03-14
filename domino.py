8369dcf2db36952803978b1dfd4b3f753f2480f7
Vivek Verma <vivnps.verma@gmail.com>
2020-03-14 10:52:16 -0700

    Domino

--- domino.py
+++ domino.py
@@ -21,31 +21,23 @@ class Chessboard(Grid):
         Grid.__init__(self, m, n, s_width=s_width, s_length=s_length, **kwargs)
         for x in range(0, m, 2):
             for y in range(0, n):
-                pos = s_width * np.array([x - m/2 + (1 if y % 2 == 0 else 1/3) * s_width, y - n/2 + s_width/3, 0])
+                pos = s_width * \
+                    np.array([x - m/2 + (1 if y % 2 == 0 else 1/3)
+                              * s_width, y - n/2 + s_width/3, 0])
                 rect = Rectangle(
-                    width=s_length, 
-                    height=s_width, 
-                    fill_opacity=0.6, 
+                    width=s_length,
+                    height=s_width,
+                    fill_opacity=0.6,
                     color=WHITE)
                 rect.shift(pos)
                 self.add(rect)
 
 
-class DominoScene(Scene):
-    CONFIG = {
-        "m": 4,
-        "n": 4,
-        "s_width": 1.5,
-        "s_length": 1.5,
-        "dt": 0.6,
-    }
-
-    def construct(self):
-        self.table = Chessboard(
-            self.m, self.n, s_width=self.s_width, s_length=self.s_length)
-        self.add(self.table)
-        for x in range(0, self.m, 2):
-            for y in range(0, self.n):
-                rect = Rectangle(width=(2*self.s_width-self.dt), height=self.s_width-self.dt, fill_opacity=1, stroke_color=WHITE, color=PURPLE).shift(
-                    [self.s_width * np.array([x - self.m/2 + self.s_length/1.5, y - self.n/2 + self.s_width/3, 0])])
+class DominoGrid(Grid):
+    def __init__(self, m, n, s_width=1, s_length=1, dt=0.6, **kwargs):
+        Grid.__init__(self, m, n, s_width=s_width, s_length=s_length, **kwargs)
+        for x in range(0, m, 2):
+            for y in range(0, n):
+                rect = Rectangle(width=(2*s_width-dt), height=s_width-dt, fill_opacity=1, stroke_color=WHITE, color=PURPLE).shift(
+                    [s_width * np.array([x - m/2 + s_length/1.5, y - n/2 + s_width/3, 0])])
                 self.add(rect)
