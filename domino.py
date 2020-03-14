1078d1fed173c75f1da8e1c71bbf63f02eba36b8
Vivek Verma <vivnps.verma@gmail.com>
2020-03-14 10:57:53 -0700

    Domino

--- domino.py
+++ domino.py
@@ -22,8 +22,9 @@ class Chessboard(Grid):
         for x in range(0, m, 2):
             for y in range(0, n):
                 pos = s_width * \
-                    np.array([x - m/2 + (1 if y % 2 == 0 else 1/3)
-                              * s_width, y - n/2 + s_width/3, 0])
+                    np.array([x - m/2 + (1 if y % 2 == 0 else 0) + 0.5,
+                              y - n/2 + 0.5,
+                              0])
                 rect = Rectangle(
                     width=s_length,
                     height=s_width,
@@ -34,10 +35,17 @@ class Chessboard(Grid):
 
 
 class DominoGrid(Grid):
-    def __init__(self, m, n, s_width=1, s_length=1, dt=0.6, **kwargs):
+    def __init__(self, m, n, s_width=1, s_length=1, dt=0.6, perm=None, **kwargs):
         Grid.__init__(self, m, n, s_width=s_width, s_length=s_length, **kwargs)
         for x in range(0, m, 2):
             for y in range(0, n):
                 rect = Rectangle(width=(2*s_width-dt), height=s_width-dt, fill_opacity=1, stroke_color=WHITE, color=PURPLE).shift(
-                    [s_width * np.array([x - m/2 + s_length/1.5, y - n/2 + s_width/3, 0])])
+                    [s_width * np.array([x - m/2 + 1, y - n/2 + 0.5, 0])])
                 self.add(rect)
+
+
+class DominoTest(Scene):
+    def construct(self):
+        #grid = DominoGrid(4, 4, s_width=1, s_length=1.5)
+        grid = Chessboard(4, 4, s_width=1.5, s_length=1.5)
+        self.play(ShowCreation(grid))
