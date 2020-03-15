880fb3d29d0f9b5d9c15912c76b573a0931e67af
Vivek Verma <vivnps.verma@gmail.com>
2020-03-14 17:23:49 -0700

    Domino

--- domino.py
+++ domino.py
@@ -4,6 +4,9 @@ from manimlib.imports import *
 class Grid(VGroup):
     def __init__(self, m, n, s_width=1, s_length=1, **kwargs):
         VGroup.__init__(self, **kwargs)
+        self.m = m
+        self.n = n
+        self.s_width = s_width
         for x in range(0, m + 1):
             self.add(Line(
                 s_width * np.array([x - m/2, -n/2, 0]),
@@ -37,15 +40,22 @@ class Chessboard(Grid):
 class DominoGrid(Grid):
     def __init__(self, m, n, s_width=1, s_length=1, dt=0.6, perm=None, **kwargs):
         Grid.__init__(self, m, n, s_width=s_width, s_length=s_length, **kwargs)
-        for x in range(0, m, 2):
-            for y in range(0, n):
-                rect = Rectangle(width=(2*s_width-dt), height=s_width-dt, fill_opacity=1, stroke_color=WHITE, color=PURPLE).shift(
-                    [s_width * np.array([x - m/2 + 1, y - n/2 + 0.5, 0])])
-                self.add(rect)
+        self.dt = dt
+        self.add_rect(2, 3)
+
+    def add_rect(self, pos1, pos2):
+        rect = Rectangle(
+            width=(2*self.s_width-self.dt),
+            height=self.s_width-self.dt,
+            fill_opacity=1,
+            stroke_color=WHITE,
+            color=PURPLE).shift(
+            [self.s_width * np.array([pos1 - self.m/2 + 1, pos2 - self.n/2 + 0.5, 0])])
+        self.add(rect)
 
 
 class DominoTest(Scene):
     def construct(self):
-        #grid = DominoGrid(4, 4, s_width=1, s_length=1.5)
-        grid = Chessboard(4, 4, s_width=1.5, s_length=1.5)
+        grid = DominoGrid(4, 4, s_width=1.5, s_length=1.5)
+        #grid = Chessboard(4, 4, s_width=1.5, s_length=1.5)
         self.play(ShowCreation(grid))
--- tiger.py
+++ tiger.py
@@ -3,8 +3,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 ydata = [3.84, 5.99, 7.81, 9.49, 11.07, 12.59, 14.07,
-         15.51, 16.92, 18.31, 19.68, 21.03, 22.36, 23.68, 25]
-xdata = [i for i in range(1, 16)]
+         15.51, 16.92, 18.31, 19.68, 21.03, 22.36, 23.68, 25,
+         26.3, 27.59, 28.87, 30.14, 31.41, 33.92, 36.42, 38.89, 41.34,
+         43.77, 46.19, 48.6, 53.38, 58.12, 62.83, 67.5]
+print(len(ydata))
+xdata = [i for i in [*range(1, 21), *range(22, 33, 2), *range(38, 51, 4)]]
+print(len(xdata))
 
 
 def linear(x, a, b):
@@ -23,18 +27,24 @@ def quad(x, a, b, c):
     return a*x ** 2 + b * x + c
 
 
-def quad2(x, a, b):
-    return a * x ** 2 + b
+def exp(x, a, b, c):
+    return a*x ** b + c
 
 
-func = quad
+func = sqrt
 l = curve_fit(func,  xdata, ydata)[0]
-x = np.linspace(1, 15)
-y = [func(i, *l) for i in x]
-n = [round(x, 2) for x in l]
-# s = "$ {0} \\sqrt {{ x }} + {1}$".format(*n)
-# s = "$ {0} x + {1} $".format(*n)
-s = "$ {0} x^2 + {1} x + {2}$".format(*n)
+
+
+def f(x): return func(x, *l)
+
+
+x = np.linspace(1, 50)
+y = [f(i) for i in x]
+n = [round(a, 2) for a in l]
+s = "$ {0} \\sqrt {{ x }} + {1}$".format(*n)
+#s = "$ {0} x + {1} $".format(*n)
+#s = "$ {0} x^2 + {1} x + {2} $".format(*n)
+#s = "$ {0} e^x + {1} $".format(*n)
 
 plt.scatter(xdata, ydata)
 plt.plot(x, y, label=s)
