edf450e2c77d6a35bc0b189cf70462415fd9dd8f
Vivek Verma <vivnps.verma@gmail.com>
2020-04-06 16:11:40 -0700

    Variaitional Calc

--- variational.py
+++ variational.py
@@ -206,3 +206,97 @@ class Difference(Scene):
         self.play(Write(eq1))
         self.play(Write(eq2))
         self.wait()
+
+
+class DistanceFunc(Scene):
+    def construct(self):
+        eq1 = TexMobject("D(f)", tex_to_color_map={
+                         "f": RED, "D": BLUE, "x": GREEN})
+        eq1.scale(1.5)
+        eq1.shift(3 * UP + 5 * LEFT)
+        head1 = TextMobject(r"Distance from \( A \) to \( B \) along \( f \)", tex_to_color_map={
+                            r"\( f \)": RED, r"\( A \)": GREEN, r"\( B \)": GREEN})
+        head1.scale(1.25)
+        head1.shift(3 * UP + 1.75 * RIGHT)
+
+        arrow = Arrow(4 * LEFT, 2.5 * LEFT, color=GOLD).shift(3 * UP)
+
+        self.play(Write(eq1), Write(arrow), FadeInFromDown(head1))
+        self.wait()
+
+        axes = Axes(
+            x_min=-3,
+            x_max=3,
+            y_min=-2,
+            y_max=2,
+            number_line_config={"include_tip": False}
+        )
+
+        tracker = ValueTracker(-1)
+
+        func = ParametricFunction(
+            self.f_r(theta=PI/4), color=RED, t_min=-2, t_max=2)
+        func.add_updater(lambda x: x.become(
+            ParametricFunction(
+                self.f_r(theta=PI/4, alpha=np.sin(tracker.get_value())), color=RED, t_min=-2, t_max=2)
+        ).shift(0.5 * DOWN))
+        grp = VGroup(axes, func)
+        grp.shift(0.5 * DOWN)
+
+        p1 = Circle(radius=0.05, fill_opacity=1, color=YELLOW)
+        p1.shift([-np.sqrt(2), -np.sqrt(2) - 0.5, 0])
+
+        p2 = Circle(radius=0.05, fill_opacity=1, color=YELLOW)
+        p2.shift([np.sqrt(2), np.sqrt(2) - 0.5, 0])
+
+        lbl1 = TexMobject("A").move_to(p1).shift(0.5 * DOWN)
+        lbl2 = TexMobject("B").move_to(p2).shift(0.5 * UP)
+
+        points = VGroup(p1, p2, lbl1, lbl2)
+
+        self.play(Write(points))
+        self.wait()
+
+        self.bring_to_back(grp)
+        self.play(Write(grp))
+        self.wait()
+
+        self.play(tracker.increment_value, 1 + 3 * PI, rate_func=linear,
+                  run_time=4*DEFAULT_ANIMATION_RUN_TIME)
+        self.wait()
+
+        f2 = ParametricFunction(
+            self.f_r(theta=PI/4), color=RED, t_min=-2, t_max=2).shift(0.5 * DOWN)
+        graph = VGroup(axes, f2, points)
+        self.add(f2)
+        self.remove(func)
+
+        self.play(ApplyMethod(graph.shift, 3.5 * LEFT))
+        eq2 = TexMobject(r"D(f) ", r"= \int_{ x_1 }^{ x_2 } \text{d}s", tex_to_color_map={
+                         "f": RED, "D": BLUE, r"\text{d}s": GREEN, "_{ x_1 }": YELLOW, "^{ x_2 }": YELLOW})
+        eq2.scale(1.25)
+        eq2.shift(1.25 * UP + 3 * RIGHT)
+
+        self.play(TransformFromCopy(eq1, eq2[:4]))
+        self.play(FadeInFromDown(eq2[4:]))
+        self.wait()
+
+        eq3 = TexMobject(r"= \int_{ x_1 }^{ x_2 } \sqrt{1 +f'(x)^2 }  \ \text{d}x", tex_to_color_map={
+                         "_{ x_1 }": YELLOW, "^{ x_2 }": YELLOW, r"\text{d}x": GREEN})
+        eq3.scale(1)
+        eq3.shift(1 * DOWN + 3 * RIGHT)
+
+        self.play(FadeInFromDown(eq3))
+        self.wait()
+
+    def f(self, x, alpha=0):
+        return alpha * np.cos(PI/4 * x)
+
+    def f_r(self, theta=PI/4, alpha=0):
+        return lambda t: [
+            t * np.cos(theta) - self.f(t, alpha=alpha) * np.sin(theta),
+            t * np.sin(theta) + self.f(t, alpha=alpha) * np.cos(theta),
+            0]
+
+    def func(self, t):
+        return [t - self.f(t), t + self.f(t), 0]
