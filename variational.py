ef80cd867dafd70016c5780f7f25e256b681d2f0
Vivek Verma <vivnps.verma@gmail.com>
2020-04-05 18:27:22 -0700

    Variational Calc

--- variational.py
+++ variational.py
@@ -12,17 +12,33 @@ class Intro(Scene):
         )
 
         f = FunctionGraph(
-            lambda x: x * np.sin(np.cos(x)) + 2,
+            self.func,
             x_min=-2,
             x_max=4
         )
-        
+
+        s = [-0.904, 0.904, 3.528]
+
         lbl = TexMobject("f(x)", color=YELLOW).move_to(f, RIGHT)
         xlbl = TexMobject("x").shift(4.5 * RIGHT)
         ylbl = TexMobject("y").shift(4.5 * UP)
 
-        grp = VGroup(axes, f, lbl, xlbl, ylbl)
+        lines = VGroup(*[self.get_lines(i) for i in s])
+        graph = VGroup(axes, f, lbl, xlbl, ylbl)
+
+        grp = VGroup(graph, lines)
         grp.move_to(2.5 * LEFT)
 
         self.add(grp)
 
+    def func(self, x):
+        return x * np.sin(np.cos(x)) + 2
+
+    def get_lines(self, point):
+        ret = VGroup()
+        ret.add(DashedLine(ORIGIN, self.func(point) * UP,
+                           dash_length=0.1, stroke_opacity=0.7).shift(point * RIGHT))
+        ret.add(Line(0.75 * LEFT, 0.75 * RIGHT,
+                     color=BLUE).shift([point, self.func(point), 0]))
+
+        return ret
