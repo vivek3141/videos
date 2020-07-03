1c0bc96f6c847dac43a9850003f64f74346987f9
Vivek Verma <vivnps.verma@gmail.com>
2020-07-03 15:49:39 -0700

    Lebesgue

--- lebesgue.py
+++ lebesgue.py
@@ -17,3 +17,88 @@ class IntroQuote(Scene):
         self.play(Write(quote))
         self.play(Write(author))
         self.wait(5)
+
+
+class FTC(GraphScene):
+    CONFIG = {
+        "x_max": 4,
+        "x_labeled_nums": list(range(-1, 5)),
+        "y_min": 0,
+        "y_max": 2,
+        "y_tick_frequency": 2.5,
+        "y_labeled_nums": list(range(5, 20, 5)),
+        "n_rect_iterations": 1,
+        "default_right_x": 3,
+        "func": lambda x: 0.1*math.pow(x-2, 2) + 1,
+        "y_axis_label": "",
+    }
+
+    def construct(self):
+        self.setup_axes()
+
+        graph = self.get_graph(self.func)
+        self.play(ShowCreation(graph))
+        self.graph = graph
+
+        rects = VGroup()
+
+        for dx in np.arange(0.2, 0.05, -0.05):
+            rect = self.get_riemann_rectangles(
+                self.graph,
+                x_min=0,
+                x_max=self.default_right_x,
+                dx=dx,
+                stroke_width=4*dx,
+            )
+            rects.add(rect)
+
+        self.play(
+            DrawBorderThenFill(
+                rects[0],
+                run_time=2,
+                rate_func=smooth,
+                lag_ratio=0.5,
+            ),
+        )
+        self.wait()
+
+        for rect in rects[1:]:
+            self.play(
+                Transform(
+                    rects[0], rect,
+                    run_time=2,
+                    rate_func=smooth,
+                    lag_ratio=0.5,
+                ),
+            )
+            self.wait()
+
+        t = Text("Riemann Integration")
+        t.scale(1.5)
+        t.shift(3 * UP)
+
+        self.play(FadeInFromDown(t))
+        self.wait()
+
+
+class Problems(Scene):
+    def construct(self):
+        title = Text(
+            "Problems with Riemann Integration", color=PURPLE)
+        title.scale(1.25)
+        title.shift(3 * UP)
+
+        l = BulletedList("Higher Dimensions", "Continuitiy",
+                         dot_color=BLUE, buff=0.75*LARGE_BUFF)
+        l.scale(1.5)
+        l.shift(0.25*DOWN)
+
+        self.play(FadeInFromDown(title))
+        self.play(Write(l))
+        self.wait()
+
+        self.play(l.fade_all_but, 0)
+        self.wait()
+
+        self.play(l.fade_all_but, 1)
+        self.wait()
