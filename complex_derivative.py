1ca5a583e50956ed38f9f6df047795179fb9109c
Vivek Verma <vivnps.verma@gmail.com>
2021-11-23 23:18:46 -0800

    Starting complex_deriv anims; NormalDerivative anim

--- complex_derivative.py
+++ complex_derivative.py
@@ -25,6 +25,8 @@ class ComplexTest(Scene):
 
 
 class NormalDerivative(Scene):
+    LINE_COLOR = "#e2e1a4"
+
     def construct(self):
         axes = Axes(x_range=(-2, 5), y_range=(0, 5))
         self.axes = axes
@@ -32,14 +34,38 @@ class NormalDerivative(Scene):
         func = axes.get_graph(self.func, color=BLUE)
         label = axes.get_graph_label(func, label=r"f(x)")
 
+        line_kwargs = {
+            "color": self.LINE_COLOR
+        }
+        eq_kwargs = {
+
+        }
         self.len_of_line = 2
+
         v = ValueTracker(-1)
-        l = self.get_line(-1)
+        l = self.get_line(-1, **line_kwargs)
+        eq = self.get_eq(-1, **eq_kwargs)
 
-        l.add_updater(lambda l: l.become(self.get_line(v.get_value())))
+        l.add_updater(lambda l: l.become(self.get_line(v.get_value(), **line_kwargs)))
+        eq.add_updater(lambda e: e.become(self.get_eq(v.get_value(), **eq_kwargs)))
 
-        self.add(axes, func, label, l)
+        self.add(axes, func, label, l, eq)
         self.embed()
+    
+    def get_eq(self, t, **kwargs):
+        f_prime = "{:.2f}".format(round(self.deriv(t), 2))
+        eq = Tex(r"{{df} \over {dx}} = ", tex_to_color_map={r"f": BLUE}, **kwargs)
+
+        n = DecimalNumber(float(f_prime), color=self.LINE_COLOR)
+        n.move_to(eq)
+        n.shift(1.25 * RIGHT)
+
+        eq = VGroup(eq, n)
+        eq.scale(1.5)
+        eq.shift(2.5 * UP)
+
+        return eq
+        
 
     def get_line(self, t, **kwargs):
         f_prime = self.deriv(t)
