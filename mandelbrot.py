b31a5650d144a319921ce7b31159becb838c060b
Vivek Verma <vivekverma@berkeley.edu>
2022-05-19 16:13:36 -0700

    Mandelbrot Shader Working; W ShaderToy

--- mandelbrot.py
+++ mandelbrot.py
@@ -1,12 +1,6 @@
 from manimlib import *
 
 
-
-
-
-
-
-
 MANDELBROT_COLORS = [
     "#00065c",
     "#061e7e",
@@ -20,130 +14,10 @@ MANDELBROT_COLORS = [
 ]
 
 
-class Scene(Scene):
-    def interact(self):
-        # If there is a window, enter a loop
-        # which updates the frame while under
-        # the hood calling the pyglet event loop
-        log.info("Tips: You are now in the interactive mode. Now you can use the keyboard"
-                 " and the mouse to interact with the scene. Just press `q` if you want to quit.")
-        try:
-            self.quit_interaction = False
-            self.lock_static_mobject_data()
-            while not (self.window.is_closing or self.quit_interaction):
-                self.update_frame(1 / self.camera.frame_rate)
-            if self.window.is_closing:
-                self.window.destroy()
-            if self.quit_interaction:
-                self.unlock_mobject_data()
-        except KeyboardInterrupt:
-            self.unlock_mobject_data()
-
-
-class Test2(Mobject):
-    CONFIG = {
-        "shader_folder": "/Users/vivek/python/videos/shaders/mandelbrot",
-    }
-
-    def __init__(self, plane, **kwargs):
-        super().__init__(
-            scale_factor=plane.get_x_unit_size(),
-            offset=plane.n2p(0),
-            **kwargs,
-        )
-        self.replace(plane, stretch=True)
-
-    def init_data(self):
-        self.data = {
-            "points": np.array([UL, DL, UR, DR]),
-        }
-
-
-class Test(Mobject):
-    CONFIG = {
-        "shader_folder": "/Users/vivek/python/videos/shaders/mandelbrot",
-        "shader_dtype": [
-            ('point', np.float32, (3,)),
-        ],
-        "scale_factor": 1.0,
-        "offset": ORIGIN,
-        "colors": MANDELBROT_COLORS,
-        "n_colors": 9,
-        "parameter": complex(0, 0),
-        "n_steps": 300,
-        "mandelbrot": True,
-    }
-
-    def __init__(self, plane, **kwargs):
-        super().__init__(
-            scale_factor=plane.get_x_unit_size(),
-            offset=plane.n2p(0),
-            **kwargs,
-        )
-        self.replace(plane, stretch=True)
-
-    def init_uniforms(self):
-        Mobject.init_uniforms(self)
-        self.uniforms["mandelbrot"] = float(self.mandelbrot)
-        self.set_parameter(self.parameter)
-        self.set_opacity(self.opacity)
-        self.set_scale(self.scale_factor)
-        self.set_colors(self.colors)
-        self.set_offset(self.offset)
-        self.set_n_steps(self.n_steps)
-
-    def init_data(self):
-        self.data = {
-            "points": np.array([UL, DL, UR, DR]),
-        }
-
-    def set_parameter(self, c):
-        self.uniforms["parameter"] = np.array([c.real, c.imag])
-        return self
-
-    def set_opacity(self, opacity):
-        self.uniforms["opacity"] = opacity
-        return self
-
-    def set_scale(self, scale_factor):
-        self.uniforms["scale_factor"] = scale_factor
-        return self
-
-    def set_colors(self, colors):
-        for n in range(len(colors)):
-            self.uniforms[f"color{n}"] = color_to_rgb(colors[n])
-        return self
-
-    def set_offset(self, offset):
-        self.uniforms["offset"] = np.array(offset)
-        return self
-
-    def set_n_steps(self, n_steps):
-        self.uniforms["n_steps"] = float(n_steps)
-        return self
-
-
-class MandelbrotTest(Scene):
-    def construct(self):
-        c = ComplexPlane()
-        t = Test2(c)
-        self.add(t)
-        self.embed()
-
-
 class MandelbrotFractal(Mobject):
     CONFIG = {
-        "shader_folder": "mandelbrot_fractal",
-        "shader_dtype": [
-            ('point', np.float32, (3,)),
-        ],
-        "scale_factor": 1.0,
-        "offset": ORIGIN,
-        "colors": MANDELBROT_COLORS,
-        "n_colors": 9,
-        "parameter": complex(0, 0),
-        "n_steps": 300,
-        "mandelbrot": True,
+        "shader_folder": "/Users/vivek/python/videos/shaders/mandelbrot",
+        "fractal_scale_factor": 2.5
     }
 
     def __init__(self, plane, **kwargs):
@@ -152,44 +26,147 @@ class MandelbrotFractal(Mobject):
             offset=plane.n2p(0),
             **kwargs,
         )
+        self.uniforms["scale_factor"] = self.fractal_scale_factor
+        self.uniforms["opacity"] = self.opacity
         self.replace(plane, stretch=True)
-
-    def init_uniforms(self):
-        Mobject.init_uniforms(self)
-        self.uniforms["mandelbrot"] = float(self.mandelbrot)
-        self.set_parameter(self.parameter)
-        self.set_opacity(self.opacity)
-        self.set_scale(self.scale_factor)
-        self.set_colors(self.colors)
-        self.set_offset(self.offset)
-        self.set_n_steps(self.n_steps)
 
     def init_data(self):
         self.data = {
             "points": np.array([UL, DL, UR, DR]),
         }
 
-    def set_parameter(self, c):
-        self.uniforms["parameter"] = np.array([c.real, c.imag])
-        return self
 
-    def set_opacity(self, opacity):
-        self.uniforms["opacity"] = opacity
-        return self
+# class Test(Mobject):
+#     CONFIG = {
+#         "shader_folder": "/Users/vivek/python/videos/shaders/mandelbrot",
+#         "shader_dtype": [
+#             ('point', np.float32, (3,)),
+#         ],
+#         "scale_factor": 1.0,
+#         "offset": ORIGIN,
+#         "colors": MANDELBROT_COLORS,
+#         "n_colors": 9,
+#         "parameter": complex(0, 0),
+#         "n_steps": 300,
+#         "mandelbrot": True,
+#     }
 
-    def set_scale(self, scale_factor):
-        self.uniforms["scale_factor"] = scale_factor
-        return self
+#     def __init__(self, plane, **kwargs):
+#         super().__init__(
+#             scale_factor=plane.get_x_unit_size(),
+#             offset=plane.n2p(0),
+#             **kwargs,
+#         )
+#         self.replace(plane, stretch=True)
 
-    def set_colors(self, colors):
-        for n in range(len(colors)):
-            self.uniforms[f"color{n}"] = color_to_rgb(colors[n])
-        return self
+#     def init_uniforms(self):
+#         Mobject.init_uniforms(self)
+#         self.uniforms["mandelbrot"] = float(self.mandelbrot)
+#         self.set_parameter(self.parameter)
+#         self.set_opacity(self.opacity)
+#         self.set_scale(self.scale_factor)
+#         self.set_colors(self.colors)
+#         self.set_offset(self.offset)
+#         self.set_n_steps(self.n_steps)
 
-    def set_offset(self, offset):
-        self.uniforms["offset"] = np.array(offset)
-        return self
+#     def init_data(self):
+#         self.data = {
+#             "points": np.array([UL, DL, UR, DR]),
+#         }
 
-    def set_n_steps(self, n_steps):
-        self.uniforms["n_steps"] = float(n_steps)
-        return self
+#     def set_parameter(self, c):
+#         self.uniforms["parameter"] = np.array([c.real, c.imag])
+#         return self
+
+#     def set_opacity(self, opacity):
+#         self.uniforms["opacity"] = opacity
+#         return self
+
+#     def set_scale(self, scale_factor):
+#         self.uniforms["scale_factor"] = scale_factor
+#         return self
+
+#     def set_colors(self, colors):
+#         for n in range(len(colors)):
+#             self.uniforms[f"color{n}"] = color_to_rgb(colors[n])
+#         return self
+
+#     def set_offset(self, offset):
+#         self.uniforms["offset"] = np.array(offset)
+#         return self
+
+#     def set_n_steps(self, n_steps):
+#         self.uniforms["n_steps"] = float(n_steps)
+#         return self
+
+
+# class MandelbrotTest(Scene):
+#     def construct(self):
+#         c = ComplexPlane()
+#         t = Test2(c)
+#         self.add(t)
+#         self.embed()
+
+
+# class MandelbrotFractal(Mobject):
+#     CONFIG = {
+#         "shader_folder": "mandelbrot_fractal",
+#         "shader_dtype": [
+#             ('point', np.float32, (3,)),
+#         ],
+#         "scale_factor": 1.0,
+#         "offset": ORIGIN,
+#         "colors": MANDELBROT_COLORS,
+#         "n_colors": 9,
+#         "parameter": complex(0, 0),
+#         "n_steps": 300,
+#         "mandelbrot": True,
+#     }
+
+#     def __init__(self, plane, **kwargs):
+#         super().__init__(
+#             scale_factor=plane.get_x_unit_size(),
+#             offset=plane.n2p(0),
+#             **kwargs,
+#         )
+#         self.replace(plane, stretch=True)
+
+#     def init_uniforms(self):
+#         Mobject.init_uniforms(self)
+#         self.uniforms["mandelbrot"] = float(self.mandelbrot)
+#         self.set_parameter(self.parameter)
+#         self.set_opacity(self.opacity)
+#         self.set_scale(self.scale_factor)
+#         self.set_colors(self.colors)
+#         self.set_offset(self.offset)
+#         self.set_n_steps(self.n_steps)
+
+#     def init_data(self):
+#         self.data = {
+#             "points": np.array([UL, DL, UR, DR]),
+#         }
+
+#     def set_parameter(self, c):
+#         self.uniforms["parameter"] = np.array([c.real, c.imag])
+#         return self
+
+#     def set_opacity(self, opacity):
+#         self.uniforms["opacity"] = opacity
+#         return self
+
+#     def set_scale(self, scale_factor):
+#         self.uniforms["scale_factor"] = scale_factor
+#         return self
+
+#     def set_colors(self, colors):
+#         for n in range(len(colors)):
+#             self.uniforms[f"color{n}"] = color_to_rgb(colors[n])
+#         return self
+
+#     def set_offset(self, offset):
+#         self.uniforms["offset"] = np.array(offset)
+#         return self
+
+#     def set_n_steps(self, n_steps):
+#         self.uniforms["n_steps"] = float(n_steps)
+#         return self
--- shaders/mandelbrot/frag.glsl
+++ shaders/mandelbrot/frag.glsl
@@ -5,6 +5,9 @@ uniform float gloss;
 uniform float shadow;
 uniform float focal_distance;
 
+//uniform vec2 parameter;
+uniform float opacity;
+
 #define product(a, b) vec2(a.x*b.x-a.y*b.y, a.x*b.y+a.y*b.x)
 #define conjugate(a) vec2(a.x,-a.y)
 #define divide(a, b) vec2(((a.x*b.x+a.y*b.y)/(b.x*b.x+b.y*b.y)),((a.y*b.x-a.x*b.y)/(b.x*b.x+b.y*b.y)))
@@ -20,20 +23,31 @@ const float X_MAX = 1.75;
 const float ASPECT_RATIO = 800.0/450.0;
 const float Y_MAX = X_MAX / ASPECT_RATIO;
 
-in vec3 xyz;
-out vec4 fragColor;
+in vec3 xyz_coords;
+out vec4 frag_color;
+
+#INSERT finalize_color.glsl
 
 void main()
 {
+    // //printf("%f %f %f\n", xyz_coords.x, xyz_coords.y, xyz_coords.z);
+    // vec3 color = vec3(xyz_coords.x/16.0 + 0.5, xyz_coords.y/8.0 + 0.5, 0.0);
+    // frag_color = finalize_color(
+    //     vec4(color, opacity),
+    //     xyz_coords,
+    //     vec3(0.0, 0.0, 1.0),
+    //     light_source_position,
+    //     gloss,
+    //     shadow
+    // );
+    // return;
     bool done = false;
-    int steps = num_steps;
-
-    vec2 fragCoord = vec2(xyz.x, xyz.y); 
+    int steps = num_steps; 
     
     vec2 curr = vec2(0, 0);
-    vec2 uv = fragCoord;
+    vec2 fragCoord = vec2(xyz_coords.x, xyz_coords.y);
+    vec3 color;
     //fragCoord = vec2(interpolate(-X_MAX, X_MAX, uv.x), interpolate(-Y_MAX, Y_MAX, uv.y));
-    
     while (steps >= 0) {
         if (sqrt(curr.x*curr.x + curr.y*curr.y) > max_arg) {
             done = true;
@@ -45,87 +59,19 @@ void main()
     
     if (done) {
         float ratio = interpolate(0.0, 1.0, float(steps)/float(num_steps));
-        fragColor = vec4(ratio, ratio, 1.0, 1.0);
+        color = vec3(ratio, ratio, 1.0);
     } else {
-        fragColor = vec4(0, 0, 0, 1.0);
+        color = vec3(0, 0, 0);
     }
+    frag_color = finalize_color(
+        vec4(color, opacity),
+        xyz_coords,
+        vec3(0.0, 0.0, 1.0),
+        light_source_position,
+        gloss,
+        shadow
+    );
+    
 }
 
 
-
-
-
-// uniform vec2 parameter;
-// uniform float opacity;
-// uniform float n_steps;
-// uniform float mandelbrot;
-
-// uniform vec3 color0;
-// uniform vec3 color1;
-// uniform vec3 color2;
-// uniform vec3 color3;
-// uniform vec3 color4;
-// uniform vec3 color5;
-// uniform vec3 color6;
-// uniform vec3 color7;
-// uniform vec3 color8;
-
-// uniform vec2 frame_shape;
-
-// in vec3 xyz_coords;
-
-// out vec4 frag_color;
-
-// #INSERT finalize_color.glsl
-// #INSERT complex_functions.glsl
-
-// const int MAX_DEGREE = 5;
-
-// void main() {
-//     frag_color = vec4(1.0, 1.0, 0.0, 1.0);
-
-//     return;
-
-//     vec3 color_map[9] = vec3[9](
-//         color0, color1, color2, color3,
-//         color4, color5, color6, color7, color8
-//     );
-//     vec3 color;
-
-//     vec2 z;
-//     vec2 c;
-
-//     if(bool(mandelbrot)){
-//         c = xyz_coords.xy;
-//         z = vec2(0.0, 0.0);
-//     }else{
-//         c = parameter;
-//         z = xyz_coords.xy;
-//     }
-
-//     float outer_bound = 2.0;
-//     bool stable = true;
-//     for(int n = 0; n < int(n_steps); n++){
-//         z = complex_mult(z, z) + c;
-//         if(length(z) > outer_bound){
-//             float float_n = float(n);
-//             float_n += log(outer_bound) / log(length(z));
-//             float_n += 0.5 * length(c);
-//             color = float_to_color(sqrt(float_n), 1.5, 8.0, color_map);
-//             stable = false;
-//             break;
-//         }
-//     }
-//     if(stable){
-//         color = vec3(0.0, 0.0, 0.0);
-//     }
-
-//     frag_color = finalize_color(
-//         vec4(color, opacity),
-//         xyz_coords,
-//         vec3(0.0, 0.0, 1.0),
-//         light_source_position,
-//         gloss,
-//         shadow
-//     );
-//  }
