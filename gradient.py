a13739e8f6adbbae753fb38a1e82d47cd1f99ff0
Vivek Verma <vivnps.verma@gmail.com>
2022-08-20 21:24:59 -0700

    Gradient; IntroGraphLeft

--- gradient.py
+++ gradient.py
@@ -35,9 +35,65 @@ class Intro(Scene):
         self.embed()
 
 
-class IntroGraphLeft(Scene):
+class IntroGraphLeft(GraphScene):
+    CONFIG = {
+        "x_max": 4,
+        "x_labeled_nums": list(range(-1, 5)),
+        "y_min": 0,
+        "y_max": 2,
+        "y_tick_frequency": 2.5,
+        "y_labeled_nums": list(range(5, 20, 5)),
+        "n_rect_iterations": 1,
+        "default_right_x": 3,
+        "func": lambda x: 0.1*math.pow(x-2, 2) + 1,
+        "y_axis_label": "",
+    }
+
     def construct(self):
-        self.embed()
+        self.setup_axes()
+
+        graph = self.get_graph(self.func)
+        self.play(ShowCreation(graph))
+        self.graph = graph
+
+        rects = VGroup()
+        for dx in np.arange(0.2, 0.05, -0.05):
+            rect = self.get_riemann_rectangles(
+                self.graph,
+                x_min=0,
+                x_max=self.default_right_x,
+                dx=dx,
+                stroke_width=4*dx,
+            )
+            rects.add(rect)
+
+        self.play(
+            DrawBorderThenFill(
+                rects[0],
+                run_time=2,
+                rate_func=smooth,
+                lag_ratio=0.5,
+            ),
+        )
+        self.wait()
+
+        for rect in rects[1:]:
+            self.play(
+                Transform(
+                    rects[0], rect,
+                    run_time=2,
+                    rate_func=smooth,
+                    lag_ratio=0.5,
+                ),
+            )
+            self.wait()
+
+        t = TexText("Riemann Integration")
+        t.scale(1.5)
+        t.shift(3 * UP)
+
+        self.play(FadeIn(t, DOWN))
+        self.wait()
 
 
 class IntroGraphRight(Scene):
